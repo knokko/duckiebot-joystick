231b0928f9b35cf3803c1b1c0770ee3b1f2d2f40
knokko <knokogator@hotmail.com>
2023-07-02 16:05:04 +0200

    Some simulator Terrain signum fixes

--- src/main/java/simulator/Terrain.java
+++ src/main/java/simulator/Terrain.java
@@ -2,7 +2,7 @@ package simulator;
 
 import java.util.function.DoubleFunction;
 
-import static java.lang.Math.max;
+import static java.lang.Math.*;
 
 public class Terrain {
 
@@ -19,7 +19,7 @@ public class Terrain {
     public static final Terrain SLOPED_SLOW = new Terrain(motorSignal -> max(0.0, 0.6 * motorSignal - 0.2));
     public static final Terrain SLOPED_FAST = new Terrain(motorSignal -> max(0.0, 1.8 * motorSignal - 0.1));
 
-    public static final Terrain NOISY_SLOW = new Terrain(motorSignal -> max(0, (0.3 * Math.random() + 0.6) * motorSignal - 0.1));
+    public static final Terrain NOISY_SLOW = new Terrain(motorSignal -> signum(motorSignal) * max(0, (0.3 * Math.random() + 0.6) * abs(motorSignal) - 0.1));
     public static final Terrain VERY_NOISY_SLOW = new Terrain(motorSignal -> max(0, (0.7 * Math.random() + 0.4) * motorSignal - 0.1));
     public static final Terrain NOISY_FAST = new Terrain(motorSignal -> (0.4 * Math.random() + 1.2) * motorSignal);
     public static final Terrain VERY_NOISY_FAST = new Terrain(motorSignal -> (0.8 * Math.random() + 0.8) * motorSignal);
