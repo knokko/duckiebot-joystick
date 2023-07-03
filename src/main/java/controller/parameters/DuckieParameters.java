ed3134ac1aa3cd28d441889dc305de43ce18554f
knokko <knokogator@hotmail.com>
2023-07-03 15:58:57 +0200

    More dirty tuning

--- src/main/java/controller/parameters/DuckieParameters.java
+++ src/main/java/controller/parameters/DuckieParameters.java
@@ -5,6 +5,8 @@ public class DuckieParameters {
     // public final PIDParameters anglePID = new PIDParameters(1.5, 0.1, 0.01);
     // public final PIDParameters speedPID = new PIDParameters(0.5, 0.0, 0.05);
 
-    public final PIDParameters anglePID = new PIDParameters(2.80, 0.0, 0.0);
+    // Skydriving + table: (1, 0, 0.3)
+    // Carpet: (3, 0, 0.3)
+    public final PIDParameters anglePID = new PIDParameters(3.00, 0.0, 0.3);
     public final PIDParameters speedPID = new PIDParameters(1.5, 0.0001, 0.5);
 }
