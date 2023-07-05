17ea2b08e02957b507431a20081dd6916c35856c
knokko <knokogator@hotmail.com>
2023-07-05 08:59:59 +0200

    Tune on Flux floor

--- src/main/java/controller/parameters/DuckieParameters.java
+++ src/main/java/controller/parameters/DuckieParameters.java
@@ -2,6 +2,6 @@ package controller.parameters;
 
 public class DuckieParameters {
 
-    public final PIDParameters anglePID = new PIDParameters(20.00, 0.0, 2.0);
+    public final PIDParameters anglePID = new PIDParameters(20.00, 0.0, 2.5);
     public final PIDParameters speedPID = new PIDParameters(1.5, 0.0001, 0.5);
 }
