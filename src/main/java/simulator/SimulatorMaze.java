c767880082c41fe3efdea1480b5dbd727aa538a3
knokko <knokogator@hotmail.com>
2023-07-05 17:06:10 +0200

    Add the 5x5 maze that we intend to use for the demo

--- src/main/java/simulator/SimulatorMaze.java
+++ src/main/java/simulator/SimulatorMaze.java
@@ -4,7 +4,7 @@ import planner.GridWall;
 
 public class SimulatorMaze {
 
-    public static WallGrid createTestingWallGrid() {
+    public static WallGrid createTestingWallGrid8x8() {
         var grid = new WallGrid();
 
         // Y = (-)4
@@ -87,6 +87,53 @@ public class SimulatorMaze {
 
         // Y = -3.5
         grid.add(new GridWall(1, -4, GridWall.Axis.Y));
+
+        return grid;
+    }
+
+    public static WallGrid createTestingWallGrid5x5() {
+        var grid = new WallGrid();
+
+        // Y = (-)3
+        for (int x = -2; x < 3; x++) {
+            grid.add(new GridWall(x, -2, GridWall.Axis.X));
+            grid.add(new GridWall(x, 3, GridWall.Axis.X));
+        }
+
+        // X = (-)3
+        for (int y = -2; y < 3; y++) {
+            grid.add(new GridWall(-2, y, GridWall.Axis.Y));
+            grid.add(new GridWall(3, y, GridWall.Axis.Y));
+        }
+
+        // Y = 2.5
+        grid.add(new GridWall(1, 2, GridWall.Axis.Y));
+
+        // Y = 2
+        grid.add(new GridWall(-1, 2, GridWall.Axis.X));
+        grid.add(new GridWall(0, 2, GridWall.Axis.X));
+
+        // Y = 1.5
+        grid.add(new GridWall(2, 1, GridWall.Axis.Y));
+
+        // Y = 1
+        grid.add(new GridWall(-2, 1, GridWall.Axis.X));
+        grid.add(new GridWall(0, 1, GridWall.Axis.X));
+        grid.add(new GridWall(1, 1, GridWall.Axis.X));
+
+        // Y = 0.5
+        grid.add(new GridWall(-1, 0, GridWall.Axis.Y));
+
+        // Y = 0
+        grid.add(new GridWall(0, 0, GridWall.Axis.X));
+
+        // Y = -0.5
+        grid.add(new GridWall(0, -1, GridWall.Axis.Y));
+        grid.add(new GridWall(2, -1, GridWall.Axis.Y));
+
+        // Y = -1
+        grid.add(new GridWall(-1, -1, GridWall.Axis.X));
+        grid.add(new GridWall(1, -1, GridWall.Axis.X));
 
         return grid;
     }
