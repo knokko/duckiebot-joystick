5ca8c378d19228c5c9658f238ed6cd076c5eaa39
knokko <knokogator@hotmail.com>
2023-07-06 16:23:12 +0200

    Minor DuckieBot constant improvement

--- src/main/java/controller/util/DuckieBot.java
+++ src/main/java/controller/util/DuckieBot.java
@@ -3,7 +3,7 @@ package controller.util;
 public class DuckieBot {
 
     public static final int WHEEL_TICKS_PER_TURN = 135;
-    public static final double DISTANCE_BETWEEN_WHEELS = 0.1;
+    public static final double DISTANCE_BETWEEN_WHEELS = 0.104;
     public static final double WHEEL_RADIUS = 0.0325;
 
     public static final double WHEEL_CIRCUMFERENCE = WHEEL_RADIUS * 2.0 * Math.PI;
